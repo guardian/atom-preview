2a3f2e277faff2afe95771e234f74ad00c2befe6
Demetrios Skamiotis <dskamiotis@gmail.com>
2021-12-17 12:09:05 +0000

    remove commented code

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -18,22 +18,5 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    */
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
-
-//    val jsFileName = "atom-preview/public/frontend/static/js/my-atom-preview-app.js"
-//    val autotrackJsLocation = routes.Assets.versioned("atom-preview/public/frontend/static/js/my-atom-preview-app.js").toString
-//
-//    val jsAssetHost = sys.env.get("JS_ASSET_HOST")
-//
-//    val isHotReloading = jsAssetHost match {
-//      case Some(_) if awsConfig.isDev => true
-//      case _ => false
-//    }
-//
-//    val jsLocation = if (isHotReloading) {
-//      jsAssetHost.get + jsFileName
-//    } else {
-//      routes.Assets.versioned(jsFileName).toString
-//    }
   }
 }
-
