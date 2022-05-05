0783cde2996d397e98ffaba5f0bcfe4f1d04aafa
Demetrios Skamiotis <dskamiotis@gmail.com>
2022-05-05 11:29:29 +0100

    update invalid user message

--- app/controllers/PanDomainAuthActions.scala
+++ app/controllers/PanDomainAuthActions.scala
@@ -23,10 +23,10 @@ trait PanDomainAuthActions extends AuthActions with Results with Logging {
 
   override def invalidUserMessage(claimedAuth: AuthenticatedUser): String = {
     if( (claimedAuth.user.emailDomain == "guardian.co.uk") && !claimedAuth.multiFactor) {
-      s"${claimedAuth.user.email} is not valid for use with Workflow as you need to have two factor authentication enabled." +
+      s"${claimedAuth.user.email} is not valid for use with config.pandaSystem as you need to have two factor authentication enabled." +
         s" Please contact the Helpdesk by emailing 34444@theguardian.com or calling 34444 and request access to Composer CMS tools"
     } else {
-      s"${claimedAuth.user.email} is not valid for use with Workflow. You need to use your Guardian Google account to login. Please sign in with your Guardian Google account first, then retry logging in"
+      s"${claimedAuth.user.email} is not valid for use with config.pandaSystem. You need to use your Guardian Google account to login. Please sign in with your Guardian Google account first, then retry logging in"
     }
   }
 }
