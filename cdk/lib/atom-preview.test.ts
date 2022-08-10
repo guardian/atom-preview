7586aecece9d7ad10398c27deaf5e109ffe58453
Demetrios Skamiotis <dskamiotis@gmail.com>
2022-08-10 18:57:18 +0100

    updates cdk version

--- cdk/lib/atom-preview.test.ts
+++ cdk/lib/atom-preview.test.ts
@@ -1,12 +1,17 @@
-import "@aws-cdk/assert/jest";
-import { SynthUtils } from "@aws-cdk/assert";
-import { App } from "@aws-cdk/core";
-import { AtomPreview } from "./atom-preview";
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { AtomPreview } from './atom-preview';
 
-describe("The AtomPreview stack", () => {
-  it("matches the snapshot", () => {
-    const app = new App();
-    const stack = new AtomPreview(app, "AtomPreview", { stack: "flexible" });
-    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
-  });
+describe('The AtomPreview stack', () => {
+	it('matches the snapshot', () => {
+		const app = new App();
+		const stack = new AtomPreview(app, 'AtomPreview', {
+			stack: 'flexible',
+			stage: 'PROD',
+			minimumInstances: 1,
+			domainName: 'atom-preview.gutools.co.uk',
+		});
+		const template = Template.fromStack(stack);
+		expect(template.toJSON()).toMatchSnapshot();
+	});
 });
