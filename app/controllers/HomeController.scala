887b3377d0b45e6ef16906a4e293e1a478cff1f6
Demetrios Skamiotis <demetrios.skamiotis@guardian.co.uk>
2022-11-16 17:55:47 +0000

    add logging for loading app

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,14 +2,14 @@ package controllers
 
 import javax.inject.Inject
 import play.api.mvc._
+import play.api.Logging
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 
-class HomeController @Inject()(cc: ControllerComponents, isDev: Boolean) extends AbstractController(cc) {
-
+class HomeController @Inject()(cc: ControllerComponents, isDev: Boolean) extends AbstractController(cc) with Logging {
   /**
    * Create an Action to render an HTML page with a welcome message.
    * The configuration in the `routes` file means that this method
@@ -17,6 +17,7 @@ class HomeController @Inject()(cc: ControllerComponents, isDev: Boolean) extends
    * a path of `/`.
    */
   def index = Action {
+    logger.info("Application is ready")
     Ok(views.html.index("Your new application is ready.", isDev))
   }
 
